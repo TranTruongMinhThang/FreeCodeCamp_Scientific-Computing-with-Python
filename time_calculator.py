34f7305ea557116fdf765f208b05270639cf4b78
Minh Thang <trantruongminhthang@gmail.com>
2022-02-07 22:31:02 +0700

    finish Time Calculator

--- time_calculator.py
+++ time_calculator.py
@@ -29,23 +29,44 @@ def time_calculator(startTime, duration, day=None):
         else:
             returnPrint += str(resultHourRaw % 24) + ':' + str(resultMinuteRaw) + ' AM'
 
-    print(returnPrint)
+    # print(returnPrint)
 
     dayOfWeekPrint = ', '
 
     dayOfWeek = {'monday': 1, 'tuesday': 2, 'wednesday': 3, 'thursday': 4, 'friday': 5, 'saturday': 6, 'sunday': 7}
     dayOfWeekReverse = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}
-
-    if resultHourRaw // 24 == 0:
-        dayOfWeek += dayOfWeekReverse[dayOfWeek[day.lower()]]
+    if day is None:
+        dayOfWeekPrint = ''
     else:
-        (dayOfWeek[day.lower()] + resultHourRaw // 24)
+        if resultHourRaw // 24 == 0:
+            dayOfWeekPrint += dayOfWeekReverse[dayOfWeek[day.lower()]]
+        else:
+            # dùng được cho ngày thứ 7 nhưng chủ nhật thì không đc
+            if (dayOfWeek[day.lower()] + resultHourRaw // 24) / 7 <= 1:
+                dayOfWeekPrint += dayOfWeekReverse[(dayOfWeek[day.lower()] + resultHourRaw // 24)]
+            elif (dayOfWeek[day.lower()] + resultHourRaw // 24) / 7 > 1:
+                dayOfWeekPrint += dayOfWeekReverse[(dayOfWeek[day.lower()] + resultHourRaw // 24) % 7]
+            else:
+                dayOfWeekPrint += dayOfWeekReverse[(dayOfWeek[day.lower()] + resultHourRaw // 24) % 7]
 
+    dayLaterPrint = ''
+    if resultHourRaw // 24 == 0:
+        dayLaterPrint += ''
+    elif resultHourRaw // 24 == 1:
+        dayLaterPrint += ' (next day)'
+    else:
+        dayLaterPrint += ' (' + str(resultHourRaw // 24) + ' days later)'
 
+    if day is None:
+        returnPrint += dayLaterPrint
+    else:
+        returnPrint += dayOfWeekPrint + dayLaterPrint
+    print(returnPrint)
 
-startTime = "3:59 PM"
-duration = "8:59"
+startTime = "23:00 PM"
+duration = "25:00"
+day = 'Sunday'
 startTimeList = startTime.split()
 # print(startTimeList[0].split(':'))
 
-time_calculator(startTime, duration)
+time_calculator(startTime, duration, day)
