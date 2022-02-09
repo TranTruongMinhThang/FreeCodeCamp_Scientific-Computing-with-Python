03267b64ba1d4eeaf30c9b79a8265a796e8b8e9a
TranTruongMinhThang <91838559+TranTruongMinhThang@users.noreply.github.com>
2022-02-09 17:01:16 +0700

    not yet done chart

--- budget.py
+++ budget.py
@@ -56,4 +56,100 @@ class Category:
 
         return titleLine + bodyLine + totalLine
 
-# def create_spend_chart(categories):
+def create_spend_chart(categories):
+    titleLine = 'Percentage spent by category'
+    totalWithdrawalOfCategory = list()
+    for i in categories:
+        sum = 0
+        for j in i.ledger:
+            if j["amount"] < 0:
+                sum += j["amount"]
+        totalWithdrawalOfCategory.append(-sum)
+
+    sumOfWithdrawal = 0
+    for i in totalWithdrawalOfCategory:
+        sumOfWithdrawal += i
+
+    percentageOfCategory = list()
+    for i in totalWithdrawalOfCategory:
+        percentageOfCategory.append(i/sumOfWithdrawal*100//10*10)
+
+    line100 = '100| '
+    line90 = ' 90| '
+    line80 = ' 80| '
+    line70 = ' 70| '
+    line60 = ' 60| '
+    line50 = ' 50| '
+    line40 = ' 40| '
+    line30 = ' 30| '
+    line20 = ' 20| '
+    line10 = ' 10| '
+    line00 = '  0| '
+    lineDash = '    ----' + '---'*(len(categories)-1)
+
+    for i in percentageOfCategory:
+        if i >= 100:
+            line100 += 'o  '
+        if i >= 90:
+            line90 += 'o  '
+        if i >= 80:
+            line80 += 'o  '
+        if i >= 70:
+            line70 += 'o  '
+        if i >= 60:
+            line60 += 'o  '
+        if i >= 50:
+            line50 += 'o  '
+        if i >= 40:
+            line40 += 'o  '
+        if i >= 30:
+            line30 += 'o  '
+        if i >= 20:
+            line20 += 'o  '
+        if i >= 10:
+            line10 += 'o  '
+        if i >= 0:
+            line00 += 'o  '
+
+    categoriesName = list()
+    for i in categories:
+        categoriesName.append(i.name)
+    # print(categoriesName)
+
+    nameLenMax = 0
+    for i in range(len(categories)):
+        if len(categoriesName[i]) > nameLenMax:
+            nameLenMax = len(categoriesName[i])
+
+    categoriesNameModifyList = list()
+    for i in categoriesName:
+        if len(i) < nameLenMax:
+            i = i + ' '*(nameLenMax - len(i))
+            categoriesNameModifyList.append(i)
+        else:
+            categoriesNameModifyList.append(i)
+
+    if len(categoriesNameModifyList) < 4:
+        for i in range(4 - len(categoriesNameModifyList)):
+            categoriesNameModifyList.append(' '*nameLenMax)
+    print(categoriesNameModifyList)
+
+
+
+    namePrint = ''
+    for i in range(nameLenMax):
+        namePrint += '     ' + categoriesNameModifyList[0][i] + '  ' + categoriesNameModifyList[1][i] + '  ' + categoriesNameModifyList[2][i] + '  ' + categoriesNameModifyList[3][i] + '\n'
+
+
+
+
+    printResult = titleLine + '\n' + line100 + '\n' + line90 + '\n' + line80 + '\n'\
+                + line70 + '\n' + line60 + '\n' + line50 + '\n' + line40 + '\n'\
+                + line30 + '\n' + line20 + '\n' + line10 + '\n' + line00 + '\n' + lineDash + '\n' \
+                + namePrint
+
+
+    return printResult
+
+
+
